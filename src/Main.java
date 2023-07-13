02a2f34be3a8fb27e8a5c7a92bcd33a44bb793d3
codetalks95 <info@codetalksdna.com>
2023-07-13 10:24:49 +0530

    Added Test and main class file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world Kavya!");
+        System.out.println("I am from main file!");
+        System.out.println("I am from main files !");
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I am from Test file!");
+        System.out.println("I am from Test file!");
+    }
+}
