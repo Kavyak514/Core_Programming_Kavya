6b2c1cee3e1223697dc924268c6699fee2f58b56
codetalks95 <info@codetalksdna.com>
2023-07-18 11:02:59 +0530

    Wrapper Primitive Values Configured.

---
+++ src/com/codetalksdna/dataTypes/PrimitiveWrapperData.java
@@ -0,0 +1,17 @@
+package com.codetalksdna.dataTypes;
+
+public class PrimitiveWrapperData {
+    public static void main(String[] args) {
+        PrimitiveWrapperDataTypes primitiveDataTypes = new PrimitiveWrapperDataTypes();
+        System.out.println(primitiveDataTypes.primitiveInt);
+        System.out.println(primitiveDataTypes.primitiveBoolean);
+        System.out.println(primitiveDataTypes.primitiveFloat);
+        System.out.println(primitiveDataTypes.primitiveLong);
+        System.out.println(primitiveDataTypes.primitiveDouble);
+        System.out.println(primitiveDataTypes.wrapperInt);
+        System.out.println(primitiveDataTypes.wrapperDouble);
+        System.out.println(primitiveDataTypes.wrapperFloat);
+        System.out.println(primitiveDataTypes.wrapperBoolean);
+        System.out.println(primitiveDataTypes.wrapperLong);
+    }
+}
---
+++ src/com/codetalksdna/dataTypes/PrimitiveWrapperDataTypes.java
@@ -0,0 +1,14 @@
+package com.codetalksdna.dataTypes;
+
+public class PrimitiveWrapperDataTypes {
+    int primitiveInt;
+    boolean primitiveBoolean;
+    double primitiveDouble;
+    long primitiveLong;
+    float primitiveFloat;
+    Integer wrapperInt;
+    Boolean wrapperBoolean;
+    Double wrapperDouble;
+    Long wrapperLong;
+    Float wrapperFloat;
+}
