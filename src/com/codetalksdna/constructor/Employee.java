0b1b3736cc9e8c8f452b50a36d398b0d87776277
codetalks95 <info@codetalksdna.com>
2023-07-19 17:00:00 +0530

    Added new Topics Code.

---
+++ src/com/codetalksdna/constructor/Employee.java
@@ -0,0 +1,14 @@
+package com.codetalksdna.constructor;
+
+public class Employee {
+    String name;
+    String age;
+
+    public Employee(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Employee() {
+    }
+}
