217cd37ab1b3c1d6ab9c31787b061fa169a51e8b
codetalks95 <info@codetalksdna.com>
2023-07-20 08:57:50 +0530

    Added Arrays Concept.

---
+++ src/com/codetalksdna/Arrays/ArraysCombination.java
@@ -0,0 +1,10 @@
+package com.codetalksdna.Arrays;
+
+public class ArraysCombination {
+    public static void main(String[] args) {
+        int arrays1[] = new int[5];
+        int[] arrays2 = new int[5];
+        int []arrays3 = new int[5];
+        int[]arrays4 = new int[5];
+    }
+}
