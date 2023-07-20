d6aa38e33ec7db27a45e633e7a8c5c921f5f9793
codetalks95 <info@codetalksdna.com>
2023-07-20 21:14:44 +0530

    Added new Packages.

--- src/CountOfProgramsDone.java
+++ src/CountOfProgramsDone.java
@@ -1,19 +1,27 @@
+import com.codetalksdna.Abstraction.CountOfFilesInAbstraction;
 import com.codetalksdna.Arrays.CountOfFilesInArrays;
+import com.codetalksdna.EncapsulationPrograms.CountOfFilesInEncapsulation;
 import com.codetalksdna.Identifier.CountOfFilesInIdentifierPackage;
 import com.codetalksdna.CodingPrograms.CountOfFilesInCodingPrograms;
 import com.codetalksdna.Constructor.CountOfFilesInConstructorPackage;
 import com.codetalksdna.DataTypes.CountOfFilesInDataTypes;
+import com.codetalksdna.InheritancePrograms.CountOfProgramInInheritance;
+import com.codetalksdna.PolymorphismPrograms.CountOfProgramInPolymorohism;
 import com.codetalksdna.UserInput.CountOfFilesInUserInputPackage;
 
 public class CountOfProgramsDone {
     public static void main(String[] args) {
+        CountOfFilesInAbstraction countOfFilesInAbstraction = new CountOfFilesInAbstraction();
         CountOfFilesInArrays countOfFilesInArrays = new CountOfFilesInArrays();
         CountOfFilesInCodingPrograms countOfFilesInCodingPrograms = new CountOfFilesInCodingPrograms();
         CountOfFilesInConstructorPackage countOfFilesInConstructorPackage = new CountOfFilesInConstructorPackage();
         CountOfFilesInDataTypes countOfFilesInDataTypes = new CountOfFilesInDataTypes();
-        CountOfFilesInIdentifierPackage countOfFilesInAbstractionUsingAbstractClass = new CountOfFilesInIdentifierPackage();
+        CountOfFilesInEncapsulation countOfFilesInEncapsulation = new CountOfFilesInEncapsulation();
+        CountOfFilesInIdentifierPackage countOfFilesInIdentifierPackage = new CountOfFilesInIdentifierPackage();
+        CountOfProgramInInheritance countOfProgramInInheritance = new CountOfProgramInInheritance();
+        CountOfProgramInPolymorohism countOfProgramInPolymorohism = new CountOfProgramInPolymorohism();
         CountOfFilesInUserInputPackage countOfFilesInUserInputPackage = new CountOfFilesInUserInputPackage();
-        int[] files = {countOfFilesInAbstractionUsingAbstractClass.returnFileCount(), countOfFilesInCodingPrograms.returnFileCount(), countOfFilesInDataTypes.returnFileCount(), countOfFilesInUserInputPackage.returnFileCount(), countOfFilesInConstructorPackage.returnFileCount(), countOfFilesInArrays.returnFileCount()};
+        int[] files = {countOfFilesInAbstraction.returnFileCount(), countOfFilesInCodingPrograms.returnFileCount(), countOfFilesInDataTypes.returnFileCount(), countOfFilesInUserInputPackage.returnFileCount(), countOfFilesInConstructorPackage.returnFileCount(), countOfFilesInArrays.returnFileCount(), countOfFilesInEncapsulation.returnFileCount(), countOfFilesInIdentifierPackage.returnFileCount(), countOfProgramInInheritance.returnFileCount(), countOfProgramInPolymorohism.returnFileCount()};
         System.out.println("Packages Created Here" + " " + files.length);
         sumOfProgramsWeDidTillNow(files);
     }
---
+++ src/com/codetalksdna/PolymorphismPrograms/CountOfProgramInPolymorohism.java
@@ -0,0 +1,13 @@
+package com.codetalksdna.PolymorphismPrograms;
+
+import PathOfProgram.PathOfProgram;
+import java.io.File;
+import java.util.Objects;
+
+public class CountOfProgramInPolymorohism {
+    public int returnFileCount() {
+        PathOfProgram pathOfProgram =  new PathOfProgram();
+        File file = new File(pathOfProgram.getPath() + "PolymorphismPrograms");
+        return Objects.requireNonNull(file.listFiles()).length;
+    }
+}
