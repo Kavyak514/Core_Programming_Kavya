239e89c8f9ef8ea9717fb6d0b65f60f3f1b1b383
codetalks95 <info@codetalksdna.com>
2023-07-21 16:51:09 +0530

    Object class added.

---
+++ src/com/codetalksdna/Objects/ObjectsExample.java
@@ -0,0 +1,9 @@
+package com.codetalksdna.Objects;
+
+import com.codetalksdna.Constructor.Employee;
+
+public class ObjectsExample {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+    }
+}
