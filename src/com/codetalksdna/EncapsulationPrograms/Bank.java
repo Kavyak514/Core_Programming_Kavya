f8496adc6d11a75142ff04f90f6c86f73f513267
codetalks95 <info@codetalksdna.com>
2023-07-25 08:58:36 +0530

    Added sample code for encapsulation.

---
+++ src/com/codetalksdna/EncapsulationPrograms/Bank.java
@@ -0,0 +1,31 @@
+package com.codetalksdna.EncapsulationPrograms;
+
+public class Bank {
+    private String bankAccountNumber;
+    private double withdrawAmount;
+    private double depositAmount;
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
+
+    public void setWithdrawAmount(double withdrawAmount) {
+        this.withdrawAmount = withdrawAmount;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+}
