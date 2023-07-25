25b023f5c6b1ae1c97962b49c97e6d49bd7fb30c
codetalks95 <info@codetalksdna.com>
2023-07-25 11:09:48 +0530

    Added sample code for encapsulation.

--- src/com/codetalksdna/EncapsulationPrograms/CustomerInterface.java
+++ src/com/codetalksdna/EncapsulationPrograms/CustomerInterface.java
@@ -7,22 +7,20 @@ public class CustomerInterface {
         InternalBankingOperations internalBankingOperations = new InternalBankingOperations();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the Account Number?");
-        String bankAccountNumberGiven = scanner.nextLine();
         internalBankingOperations.setBankAccountNumber("123456");
-        if (internalBankingOperations.getBankAccountNumber().equals(bankAccountNumberGiven)) {
+        if (internalBankingOperations.getBankAccountNumber().equals(scanner.nextLine())) {
             System.out.println("You are Authenticated , Please Enter the Amount you wish to deposit?");
             internalBankingOperations.setDepositAmount(scanner.nextInt());
             System.out.println("Your Transaction Of Depositing Money is Successful and Your Current Balance is" + " " + internalBankingOperations.getDepositAmount());
-            System.out.println("Do you wish to Proceed Further or Do You Wish to Quit, Please Enter in Yes or No");
-            String decision = scanner.next();
-            if (decision.toUpperCase().equals("YES")) {
+            System.out.println("Do you wish to Proceed Further or Do You Wish to Quit, Please Enter in Yes or No.");
+            if (scanner.next().equalsIgnoreCase("YES")) {
                 System.out.println("Enter the amount you wish to Withdraw?");
                 internalBankingOperations.setWithdrawAmount(scanner.nextInt());
             } else {
-                System.out.println("Thanks for Banking with us");
+                System.out.println("Thanks for Banking with us.");
             }
         } else {
-            System.out.println("Please Check your account Number");
+            System.out.println("Please Check your account Number and retry Again.");
         }
     }
 }
