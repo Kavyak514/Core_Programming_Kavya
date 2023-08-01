d25a16451eb0a3dcd35496410890911cd8797818
kavyakonkati <83880566+kavyakonkati@users.noreply.github.com>
2023-08-01 08:11:50 +0530

    Default methods in interface and encapsulation

--- src/com/codetalksdna/CodingPrograms/ExceptionHandling1.java
+++ src/com/codetalksdna/CodingPrograms/ExceptionHandling1.java
@@ -4,7 +4,7 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ExceptionHandling1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException {
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter the first number");
@@ -12,6 +12,9 @@ public class ExceptionHandling1 {
             System.out.println("Please enter the second number");
             int b = scanner.nextInt();
             int c = a / b;
+            int[] a1 = new int[2];
+            a1[2] = 3;
+            System.out.println("test");
             System.out.println("division of two numbers is " + c);
         }catch (Exception e){
             e.printStackTrace();
