8ef6c86c3434a2ef5bcbdf117433bc33428cf24f
codetalks95 <info@codetalksdna.com>
2023-08-02 09:21:24 +0530

    Added Serialization.

---
+++ src/com/codetalksdna/Serialization/DeSerializationClass.java
@@ -0,0 +1,17 @@
+package com.codetalksdna.Serialization;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class DeSerializationClass {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("abc.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Student student = (Student) objectInputStream.readObject();
+        System.out.println(student.name);
+        System.out.println(student.rollNo);
+        objectInputStream.close();
+        fileInputStream.close();
+    }
+}
