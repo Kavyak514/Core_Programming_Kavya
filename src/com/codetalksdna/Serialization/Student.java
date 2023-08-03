4dd3cccf836c919473f7c0d41cf543698a4d4456
codetalks95 <info@codetalksdna.com>
2023-08-03 09:16:40 +0530

    Added Serialization Concept.

--- src/com/codetalksdna/Serialization/Student.java
+++ src/com/codetalksdna/Serialization/Student.java
@@ -3,8 +3,8 @@ package com.codetalksdna.Serialization;
 import java.io.Serializable;
 
 public class Student implements Serializable {
-    private static final long serialVersionUID=123456789L;
-    String name;
+    private static final long serialVersionUID = 123456789L;
+    static String name;
     String rollNo;
     String userName;
     transient String password;
