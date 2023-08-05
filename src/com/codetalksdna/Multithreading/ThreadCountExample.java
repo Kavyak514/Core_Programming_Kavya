afb27e5a653954bcdab1f921dca4974746da259b
codetalks95 <info@codetalksdna.com>
2023-08-05 17:10:19 +0530

    Fixed Issue with thread count.

---
+++ src/com/codetalksdna/Multithreading/ThreadCountExample.java
@@ -0,0 +1,25 @@
+package com.codetalksdna.Multithreading;
+
+public class ThreadCountExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            Thread thread = new Thread(new MyRunnable());
+            thread.setDaemon(true);
+            thread.start();
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main thread exiting.");
+    }
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Thread count: " + Thread.activeCount());
+        }
+    }
+}
